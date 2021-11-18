419fb35288685afb79750bc1aafffa31bdb2a63c
BryantCR <bryancvargas20@gmail.com>
2021-11-18 12:37:53 -0600

    All ok

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer(function( request, response ){
     console.log('client request URL: ', request.url);
 
 	if( request.url === "/cars" ){
-		fs.readFile('cars.html', 'utf-8', function(errors, contents){
+		fs.readFile('./views/cars.html', 'utf-8', function(errors, contents){
 			response.writeHead(200, {'Content-Type': 'text/html'} );
 				response.write( contents );
 				response.end();
@@ -13,13 +13,13 @@ const server = http.createServer(function( request, response ){
 	}
 
     else if( request.url === "/cats" ){
-		fs.readFile('cats.html', 'utf-8', function(errors, contents){
+		fs.readFile('./views/cats.html', 'utf-8', function(errors, contents){
 			response.writeHead(200, {'Content-Type': 'text/html'} );
 				response.write( contents );
 				response.end();
 		})
 	}
-
+/*
     else if( request.url === "/cars/new" ){
 		fs.readFile('dojos.html', 'utf-8', function(errors, contents){
 			response.writeHead(200, {'Content-Type': 'text/html'} );
@@ -27,7 +27,7 @@ const server = http.createServer(function( request, response ){
 				response.end();
 		})
 	}
-
+*/
 	else{
 		response.writeHead( 404 );
 		response.end( "Route Not Found!" );
@@ -37,4 +37,4 @@ const server = http.createServer(function( request, response ){
 
 server.listen( 7077 );
 
-console.log("Running in localhost at port 8080");
+console.log("Running in localhost at port 7077");
